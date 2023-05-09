f121133cc539f4f38b06ca712ce6c75f0390fd2b
Chinmayi Hegde <42185493+chinhegde@users.noreply.github.com>
2023-05-09 12:08:09 -0700

    Revert "Updates in classes"

--- fake-net.go
+++ fake-net.go
@@ -1,11 +1,3 @@
 package blockchain
 
-type FakeNet struct {
-	clients map[string]interface{}
-}
-
-func (fn *FakeNet) Register(clientList ...*Client) {
-	for _, client := range clientList {
-		fn.clients[client.address] = client
-	}
-}
+type Fakenet struct{}
--- utils/utils.go
+++ utils/utils.go
@@ -1,52 +1,5 @@
 package utils
 
-import (
-	"crypto"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/sha256"
-	"encoding/base64"
-	"encoding/hex"
-	"encoding/json"
-)
-
-const (
-	HASH_ALG       = crypto.SHA256
-	SIG_ALG        = crypto.SHA256 // there is no equivalent RSA-SHA256 in Go
-	MODULUS_LENGTH = 512
-)
-
-//	func Hash(s string) string {
-//		return "" //TODO
-//	}
-func Hash(s string) []byte {
-	hash := crypto.Hash(HASH_ALG).New()
-	hash.Write([]byte(s))
-	return hash.Sum(nil)
-}
-
-func calcAddress(key string) string {
-	hash := sha256.Sum256([]byte(key))
-	addr := base64.StdEncoding.EncodeToString(hash[:])
-	return addr
-}
-
-func Sign(privKey []byte, msg interface{}) (string, error) {
-	signer := rsa.PrivateKey{}
-	if err := json.Unmarshal(privKey, &signer); err != nil {
-		return "", err
-	}
-
-	msgBytes, err := json.Marshal(msg)
-	if err != nil {
-		return "", err
-	}
-
-	hashed := sha256.Sum256(msgBytes)
-	signature, err := signer.Sign(rand.Reader, hashed[:], SIG_ALG)
-	if err != nil {
-		return "", err
-	}
-
-	return hex.EncodeToString(signature), nil
+func Hash(s string) string {
+	return "" //TODO
 }
